bb55d62927613f6bc2624a1fa0c67189423c4bef
Mike Lyons <lyons.mr@gmail.com>
2013-10-16 16:50:33 -0700

    turn this into a rails app

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,7 @@
+class MapsController < ApplicationController
+
+  # GET /map/:map_id
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmokeSignals::Application.config.secret_token = '689597d670a55ac9878c5d27e8a87a0fa3eb9ca59f4985d823bc7b7a7260574e4d3b5cbe24c45b95a5fcf5c99778597aebe3ad297d7598a9d1af1bf8b28c175c'
